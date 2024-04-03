a3f94ea3bdb7bef90b189785b3e3f3dff5c729b9
flyingking888 <jeremyfeiwang@outlook.com>
2024-04-02 21:54:06 -0400

    Initial Commit

---
+++ Flagler CIS-DS App/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flagler CIS-DS App
+//
+//  Created by Jeremy Wang on 4/2/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
